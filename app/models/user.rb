f4bb9674f3c32527feb051a459ab151e0bc01e8e
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-01-20 20:50:52 +0200

    Add user constraint

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: 'author_id'
     has_many :comments, foreign_key: 'author_id'
     has_many :likes, foreign_key: 'author_id'
+
+    def three_latest_posts
+        posts.order('created_at DESC').limit(3)
+    end
   
 end
