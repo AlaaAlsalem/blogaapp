55f62221b0fcd1bf42e1803bdb1d8be5177cbed1
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-01-25 09:29:27 +0200

    Fix Rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
-# config/routes.rb
-
 Rails.application.routes.draw do
-  # For the index action
+ 
   get '/user', to: 'user#index', as: 'users'
   
-  # For the show action
   get '/user/:id', to: 'user#show', as: 'user'
 
   get '/posts/', to: 'post#index'
   get '/posts/show', to: 'post#show'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
 end
