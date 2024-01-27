1f467256f8ae7ccf3497848592f8ad54beb08c5d
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-01-27 21:08:24 +0200

    Fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,6 @@ class Post < ApplicationRecord
   validates :ComentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-
   def five_latest_comments
     comments.order('created_at DESC').limit(5)
   end
