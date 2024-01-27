02a8669f22d38bd4a9367810ee0153bd48b96cf7
AlaaAlsalem <alsalem.aladdin@gmail.com>
2024-01-27 20:54:14 +0200

    Fix attribute typing errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,14 @@ class Post < ApplicationRecord
   validates :ComentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+
+  def five_latest_comments
+    comments.order('created_at DESC').limit(5)
+  end
+
   private
 
   def update_posts_counter
     author.increment!(:post_counter)
-  end
-
-  def five_latest_comments
-    comments.order('created_at DESC').limit(5)
   end
 end
